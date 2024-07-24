9546bbef8bf51b0ebe12b3ce0e1ca33bbe8d7788
Leon Schourek <Leon.Schourek@rhenus.com>
2024-07-24 08:12:31 +0200

    init

---
+++ ProductpData.cs
@@ -0,0 +1,19 @@
+﻿namespace AotNCalcTest
+{
+	public class ProductpData
+	{
+		public ProductpData(uint price, float mwst)
+		{
+			this.Price = price;
+			this.Mwst = mwst;
+		}
+
+		public uint Price { get; set; }
+
+		public float Mwst { get; set; }
+
+		public uint MwstMoney => (uint)Math.Round(this.Price * this.Mwst);
+
+		public ulong FullPrice => (ulong)Math.Round(this.Price * (this.Mwst + 1));
+	};
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+using AotNCalcTest;
+
+using NCalc;
+
+internal class Program
+{
+	//[DynamicDependency(DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties, typeof(IDiscountProductGroupData))]
+	private static void Main(string[] args)
+	{
+		ProductpData p = new ProductpData(1900, 0.19f);
+		string expr = "[FullPrice]-([Price]*0.9*(1+[Mwst]))";
+		NCalc.Expression exp = new Expression(expr);
+		if (exp.HasErrors())
+		{
+			throw exp.Error;
+		}
+
+		exp.Options = ExpressionOptions.OverflowProtection | ExpressionOptions.DecimalAsDefault;
+		Console.WriteLine(Create1<ProductpData>(exp)(p));
+	}
+
+	private static Func<T, ulong> Create0<T>(Expression exp) => exp.ToLambda<T, ulong>();
+
+	private static Func<T, ulong> Create1<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields | DynamicallyAccessedMemberTypes.PublicProperties)] T>(Expression exp) => exp.ToLambda<T, ulong>();
+}
